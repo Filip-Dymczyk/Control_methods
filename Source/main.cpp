1490a09c7ca6f7ee2685033404ce12a41b8546d8
unknown <filip.dymczyk@o2.pl>
2024-09-01 22:41:37 +0200

    Tested maths behind RLS.

--- Source/main.cpp
+++ Source/main.cpp
@@ -23,74 +23,75 @@ int main()
     // test.test_component<PID>(pid, &ramp);
     // test.test_open_loop_control<ObjectStandardRepresentation<2>>(object_open_loop, pid, &ramp);
     // test.test_closed_loop_control<ObjectStandardRepresentation<2>>(object_unstable, pid, &ramp);
-    using VectorT = std::array<double, 2>;
-    using MatrixT = std::array<std::array<double, 2>, 2>;
-    MatrixT diagonal = create_diagonal_matrix<MatrixT>();
-    for(auto const & row : diagonal)
-    {
-        for(auto const & elem : row)
-        {
-            std::cout << elem << ", ";
-        }
-        std::cout << "\n";
-    }
+    // using VectorT = std::array<double, 2>;
+    // using MatrixT = std::array<std::array<double, 2>, 2>;
+    // MatrixT diagonal = create_diagonal_matrix<MatrixT>();
+    // for(auto const & row : diagonal)
+    // {
+    //     for(auto const & elem : row)
+    //     {
+    //         std::cout << elem << ", ";
+    //     }
+    //     std::cout << "\n";
+    // }
 
-    VectorT v {1.0, 2.0};
-    MatrixT m {{{1.0, 2.0}, {1.0, 2.0}}};
-    VectorT out = matrix_vector_multiplication_vector_product<MatrixT, VectorT>(m, v);
-    for(auto const & elem : out)
-    {
-        std::cout << elem << "\n";
-    }
+    // VectorT v {1.0, 2.0};
+    // MatrixT m {{{1.0, 2.0}, {1.0, 2.0}}};
+    // VectorT out = matrix_vector_multiplication_vector_product<MatrixT, VectorT>(m, v);
+    // for(auto const & elem : out)
+    // {
+    //     std::cout << elem << "\n";
+    // }
 
-    std::cout << vectors_multiplication_scalar_product<std::array<double, 2>>(v, v) << "\n";
-    scale_vector(out, 0.2);
-    for(auto const & elem : out)
-    {
-        std::cout << elem << "\n";
-    }
+    // std::cout << vectors_multiplication_scalar_product<std::array<double, 2>>(v, v) << "\n";
+    // scale_vector(out, 0.2);
+    // for(auto const & elem : out)
+    // {
+    //     std::cout << elem << "\n";
+    // }
 
-    VectorT adding_result = add_vectors(out, v);
-    for(auto const & elem : adding_result)
-    {
-        std::cout << elem << "\n";
-    }
+    // VectorT adding_result = add_vectors(out, v);
+    // for(auto const & elem : adding_result)
+    // {
+    //     std::cout << elem << "\n";
+    // }
 
-    MatrixT mul_result = vectors_multiplication_matrix_product<MatrixT, VectorT>(v, v);
-    for(auto const & row : mul_result)
-    {
-        for(auto const & elem : row)
-        {
-            std::cout << elem << ", ";
-        }
-        std::cout << "\n";
-    }
+    // MatrixT mul_result = vectors_multiplication_matrix_product<MatrixT, VectorT>(v, v);
+    // for(auto const & row : mul_result)
+    // {
+    //     for(auto const & elem : row)
+    //     {
+    //         std::cout << elem << ", ";
+    //     }
+    //     std::cout << "\n";
+    // }
 
-    MatrixT subtract_result = subtract_matrices<MatrixT>(mul_result, m);
-    for(auto const & row : subtract_result)
-    {
-        for(auto const & elem : row)
-        {
-            std::cout << elem << ", ";
-        }
-        std::cout << "\n";
-    }
+    // MatrixT subtract_result = subtract_matrices<MatrixT>(mul_result, m);
+    // for(auto const & row : subtract_result)
+    // {
+    //     for(auto const & elem : row)
+    //     {
+    //         std::cout << elem << ", ";
+    //     }
+    //     std::cout << "\n";
+    // }
 
-    scale_matrix<MatrixT>(subtract_result, 2.0);
-    for(auto const & row : subtract_result)
-    {
-        for(auto const & elem : row)
-        {
-            std::cout << elem << ", ";
-        }
-        std::cout << "\n";
-    }
+    // scale_matrix<MatrixT>(subtract_result, 2.0);
+    // for(auto const & row : subtract_result)
+    // {
+    //     for(auto const & elem : row)
+    //     {
+    //         std::cout << elem << ", ";
+    //     }
+    //     std::cout << "\n";
+    // }
 
-    VectorT mul_product = vector_matrix_multiplication_vector_product<VectorT, MatrixT>(v, m);
-    for(auto const & elem : mul_product)
-    {
-        std::cout << elem << "\n";
-    }
-
+    // VectorT mul_product = vector_matrix_multiplication_vector_product<VectorT, MatrixT>(v, m);
+    // for(auto const & elem : mul_product)
+    // {
+    //     std::cout << elem << "\n";
+    // }
+    RecursiveLinearRegression<2> regression(2.0);
+    regression.update({1.0, 1.0}, {1.0, 2.0}, 1.0, 0.0);
     return 0;
 }
