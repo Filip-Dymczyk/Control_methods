b92c160935e514ebbe782bf5ec70a7f5bb448b2d
unknown <filip.dymczyk@o2.pl>
2024-09-01 23:55:44 +0200

    Making testing with plots compatible with signals change.

--- Source/test/test_with_plot.h
+++ Source/test/test_with_plot.h
@@ -23,7 +23,7 @@ class TestWithPlot
     };
 
 public:
-    TestWithPlot(double sim_time, double time_step) : _sim_time(sim_time), _time_step(time_step) {}
+    TestWithPlot(double sim_time) : _sim_time(sim_time) {}
 
     template<typename ObjectT>
     void
@@ -31,7 +31,7 @@ public:
     {
         PlottingBuffers const buffers = simulate_open_closed_loop(object, pid, input_signal, ControlMode::CLOSED_LOOP);
 
-        plot_test(buffers.time, buffers.set_point, buffers.control, buffers.output, ControlMode::CLOSED_LOOP, plot_control);
+        plot_test(buffers, ControlMode::CLOSED_LOOP, plot_control);
     }
 
     template<typename ObjectT>
@@ -40,7 +40,7 @@ public:
     {
         PlottingBuffers const buffers = simulate_open_closed_loop(object, pid, input_signal, ControlMode::OPEN_LOOP);
 
-        plot_test(buffers.time, buffers.set_point, buffers.control, buffers.output, ControlMode::OPEN_LOOP, plot_control);
+        plot_test(buffers, ControlMode::OPEN_LOOP, plot_control);
     }
 
     template<typename ComponentT>
@@ -49,7 +49,7 @@ public:
     {
         PlottingBuffers const buffers = simulate_component(component, input_signal);
 
-        plot_test(buffers.time, buffers.set_point, buffers.control, buffers.output, ControlMode::NONE, false);
+        plot_test(buffers, ControlMode::NONE, false);
     }
 
     void
@@ -57,16 +57,8 @@ public:
     {
         _sim_time = sim_time;
     }
-
-    void
-    set_time_step(double time_step)
-    {
-        _time_step = time_step;
-    }
-
 private:
     double _sim_time {};
-    double _time_step {};
 
     template<typename ObjectT>
     PlottingBuffers const
@@ -79,16 +71,14 @@ private:
         std::vector<double> control {};
         std::vector<double> output {};
         
-        double t = 0;
-        while(t < _sim_time)
+        while(input_signal -> time() < _sim_time)
         {
-            time.push_back(t);
+            time.push_back(input_signal -> time());
             control_loop.update(input_signal -> get_value());
             set_point.push_back(input_signal -> get_value());
             control.push_back(control_loop.get_control());
             output.push_back(control_loop.get_output());
-            t += _time_step;
-            input_signal -> update(t);
+            input_signal -> update();
         }  
         return {time, set_point, control, output};
     }
@@ -102,21 +92,19 @@ private:
         std::vector<double> set_point {};
         std::vector<double> output {};
 
-        double t = 0;
-        while(t < _sim_time)
+        while(input_signal -> time() < _sim_time)
         {
-            time.push_back(t);
+            time.push_back(input_signal -> time());
             object.update(input_signal -> get_value());   
             set_point.push_back(input_signal -> get_value());
             output.push_back(object.get_value());
-            t += _time_step;
-            input_signal -> update(t);
+            input_signal -> update();
         }  
         return {time, set_point, {}, output};
     }
 
     void
-    plot_test(std::vector<double> const & time, std::vector<double> const & set_point, std::vector<double> const & control, std::vector<double> const & output, ControlMode const & control_mode, bool plot_control)
+    plot_test(PlottingBuffers const & buffers, ControlMode const & control_mode, bool plot_control)
     {
         plt::figure();
         switch(control_mode) 
@@ -138,12 +126,12 @@ private:
             }
         } 
         
-        plt::plot(time, set_point, "r-", {{"label", "set point"}});
+        plt::plot(buffers.time, buffers.set_point, "r-", {{"label", "set point"}});
         if(plot_control)
         {
-            plt::plot(time, control, "y-", {{"label", "control"}});
+            plt::plot(buffers.time, buffers.control, "y-", {{"label", "control"}});
         }
-        plt::plot(time, output, "b-", {{"label", "output"}});
+        plt::plot(buffers.time, buffers.output, "b-", {{"label", "output"}});
         plt::xlabel("Time [s]");
         plt::ylabel("y");
         plt::legend("best");
