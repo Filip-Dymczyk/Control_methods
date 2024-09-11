534381f65cacb9f7e2238f85e3f628643330af56
unknown <filip.dymczyk@o2.pl>
2024-09-11 17:01:58 +0200

    Signals tests

--- Tests/test_suites/signals_test.cpp
+++ Tests/test_suites/signals_test.cpp
@@ -7,8 +7,11 @@
 
 class SignalsTest : public testing::Test, public TestWithPlot
 {
-    double const time_step = 0.1;
-    Heaviside heaviside {time_step};
+    double const _time_step = 0.001;
+    Heaviside heaviside {_time_step};
+    Ramp ramp {_time_step};
+    Rectangle rect {_time_step, 3.0};
+    SineWave sine_wave {_time_step, 1.0, 1.0};
 protected:
     SignalsTest() : TestWithPlot(15.0) {}
 
@@ -17,9 +20,42 @@ protected:
     {
         test_signal(&heaviside, signal_plot_title);
     }
+
+    void
+    test_ramp(std::string signal_plot_title)
+    {
+        test_signal(&ramp, signal_plot_title);
+    }
+
+    void
+    test_rect(std::string signal_plot_title)
+    {
+        test_signal(&rect, signal_plot_title);
+    }
+
+    void
+    test_sine_wave(std::string signal_plot_title)
+    {
+        test_signal(&sine_wave, signal_plot_title);
+    }
 };
 
 TEST_F(SignalsTest, HeavisideTest)
 {
     test_heaviside("Heaviside signal");
+}
+
+TEST_F(SignalsTest, RampTest)
+{
+    test_ramp("Ramp signal");
+}
+
+TEST_F(SignalsTest, RectangleTest)
+{
+    test_rect("Rectangle signal");
+}
+
+TEST_F(SignalsTest, SineWaveTest)
+{
+    test_sine_wave("Sinusoidal signal");
 }
