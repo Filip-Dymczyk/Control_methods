50936855d1246080eea4071b72504ec4b14b054d
unknown <filip.dymczyk@o2.pl>
2024-09-20 17:17:53 +0200

    Minor changes././

--- Source/sim_objects/object_state_space_representation.h
+++ Source/sim_objects/object_state_space_representation.h
@@ -10,7 +10,7 @@
 //              x' = Ax + Bu
 //              y  = Cx + Du
 // where: n - number of state variables,
-// A is n x n matrix, B is n x 1 vector, C is 1 x n transposed vector D is a scalar (default equals to 0).
+// A is [n x n] matrix, B is [n x 1] vector, C is [1 x n] transposed vector D is a scalar (by default set to 0).
 // We allow entering whole matrices / vectors with coefficients.
 // When entering coeficients beware of mistakingly creating unstable objects!
 // We allow to set up desired initial conditions in a manner: {x'(0), x(0)}.
@@ -22,7 +22,7 @@ public:
     using MatrixT = std::array<std::array<double, order>, order>;
     using VectorT = std::array<double, order>;
 
-    ObjectStateSpaceRepresentation(double time_step, MatrixT const & A, VectorT const & B, VectorT const & C, double D, initial_state const & init_state) : SimumlationObjectBase(time_step), _state(time_step, init_state), _A(A), _B(B), _C(C), _D(D) {}
+    ObjectStateSpaceRepresentation(double time_step, MatrixT const & A, VectorT const & B, VectorT const & C, double D = 0.0, initial_state const & init_state) : SimumlationObjectBase(time_step), _state(time_step, init_state), _A(A), _B(B), _C(C), _D(D) {}
 
     void 
     update(double control) override
