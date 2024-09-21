f664acc627879ff2bf8a2b07819ab8814994ba7d
unknown <filip.dymczyk@o2.pl>
2024-09-21 22:42:49 +0200

    Beginning simple gui app development.

--- MainApp/main.cpp
+++ MainApp/main.cpp
@@ -1,7 +1,15 @@
 // Control methods for linear SISO dynamical systems.
 // Author: Filip Dymczyk
 
-int main()
-{
-    return 0;
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+
+    QPushButton button("Hello, World!");
+    button.resize(200, 100);
+    button.show();
+
+    return app.exec();
 }
