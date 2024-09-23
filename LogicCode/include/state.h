544230c26b4a237b5e124c07109946bf0ca01593
unknown <filip.dymczyk@o2.pl>
2024-09-23 20:14:36 +0200

    Usage of vectors.

--- LogicCode/include/state.h
+++ LogicCode/include/state.h
@@ -4,52 +4,54 @@
 #pragma once
 
 #include <cstdint>
-#include <array>
+#include <vector>
 #include <assert.h>
 #include "integrator.h"
 
-template<std::size_t order>
 class State
 {
 public:
-    using initial_state = std::array<double, order>;
-
-    State(double time_step, initial_state const & init_state)
+    State(double time_step, std::uint32_t order, std::vector<double> const & init_state)
     {
         assert(order > 0u);
-
-        for(std::size_t i = 0; i < order; i++)
+        assert(order == init_state.size());
+        _order = order;
+        for(const auto & state : init_state)
         {
-            Integrator I {time_step, init_state[i]};
-            _integrators[i] = I;
+            Integrator I {time_step, state};
+            _integrators.push_back(I);
         }
     }
 
     double
     get_value(std::size_t idx) const
     {
-        return _integrators[idx].get_value();
+        assert(idx < _integrators.size());
+        return _integrators.at(idx).get_value();
     }
 
     void
-    update(double highest_order_deriavtive)
+    update(double highest_order_derivative)
     {
+        assert(_order == _integrators.size());
         // Next integrals of state derivatives:
-        _integrators[0].update(highest_order_deriavtive);
-        for(std::size_t i = 1; i < order; i++)
+        _integrators.at(0).update(highest_order_derivative);
+        for(std::size_t i = 1; i < _order; i++)
         {
-            _integrators[i].update(_integrators[i - 1].get_value());
+            _integrators.at(i).update(_integrators.at(i - 1).get_value());
         }
     }
 
     void
-    update(std::array<double, order> const & new_state_derivative)
+    update(std::vector<double> const & new_state_derivative)
     {
-        for(std::size_t i = 0; i < order; i++)
+        assert(_order == new_state_derivative.size());
+        for(std::size_t i = 0; i < _order; i++)
         {
-            _integrators[i].update(new_state_derivative[i]);
+            _integrators.at(i).update(new_state_derivative.at(i));
         }
     }
 private:
-    std::array<Integrator, order> _integrators;
+    std::uint32_t _order {};
+    std::vector<Integrator> _integrators;
 };
